79d80f826f456a5268ee51f318536037d7d76371
Zach Tatlock <ztatlock@cs.ucsd.edu>
2010-12-08 11:35:12 -0800

    add file reading and the fast forward operator
    readlines takes a filename and returns a list of its lines
    
    file_str collapses this list into a newline delimited string
    
    ff (fast forward) operator:
      take a function and a value
      apply the function to the value
      ignore the result
      return the value

--- src/common.ml
+++ src/common.ml
@@ -2,11 +2,18 @@
 module ZPervasives = struct
   let (|>) x f = f x
 
+  let ff f x =
+    f x |> ignore;
+    x
+
   let tick = ref 0
 
   let tock () =
     tick := !tick + 1;
     !tick
+
+  let printf = Printf.printf
+  let mkstr  = Printf.sprintf
 end
 
 open ZPervasives
@@ -17,4 +24,21 @@ let pair_up l rs =
 let xprod ls rs =
   ls |> List.map (fun l -> pair_up l rs)
      |> List.flatten
+
+let readlines file =
+  let f = open_in file in
+  let rec loop ls =
+    let next =
+      try Some (input_line f)
+      with End_of_file -> None
+    in match next with
+    | None   -> List.rev ls
+    | Some l -> loop (l::ls)
+  in
+  let ls = loop [] in
+  close_in f;
+  ls
+
+let file_str f =
+  String.concat "\n" (readlines f)
 
