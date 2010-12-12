5e11ca87582e642fea13077f5f34f18405cd5914
Zach Tatlock <ztatlock@cs.ucsd.edu>
2010-12-12 09:49:14 -0800

    initial test infrastructure for relate checker
    script to test relational checker, based on parser checker script
    
    cleaned up existing tests and added a few additional

---
+++ script/test-relate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source $BOUNCER/script/common
+
+TEST="$BOUNCER/test/relate/*.rwr"
+OUTP="$BOUNCER/output/relate"
+
+function run {
+  pec $1 \
+   1> $OUTP-$2 \
+   2> $OUTP-$2-error
+  tail -n 1 $OUTP-$2
+}
+
+function check {
+  expect=$(echo $2 | sed 's/^\(.\).*/\1/')
+  actual=$(run $1 $2)
+  ( [ "$expect" = "p" ] && [ "$actual" = "VALID"   ] ) || \
+  ( [ "$expect" = "n" ] && [ "$actual" = "INVALID" ] )
+}
+
+for t in $TEST; do
+  nm=$(basename $t .rwr)
+  printf "%-20s " $nm
+  if check $t $nm; then
+    echo $PASS
+  else
+    echo $FAIL
+  fi
+done
+
