f3e4c09d9ac0a57e94e65ab0b93b37bfe4643c70
Zach Tatlock <ztatlock@cs.ucsd.edu>
2010-12-22 15:22:08 -0800

    reworked checker's toplevel strategy
    add all path pairs to queue
    
    while not queue.empty():
    
      pp = queue.pop()
    
      if not check(pp):
    
        add all path pairs ending at pp's entry to queue
    
    ---
    
    Also added a mechanism to limit the number of times simrel is strengthened.

--- src/check.ml
+++ src/check.ml
@@ -36,10 +36,28 @@ let obligation rwr pp =
   in
   Logic.imply (Logic.conj hyps) bot
 
+(* track # of strengthenings and terminate upon reaching limit *)
+
+exception GiveUp
+
+let nstrength =
+  ref 0
+
+let reset_nstrength () =
+  nstrength := 0
+
+let too_strong () =
+  let n =
+    "strength" |> Flags.get
+               |> int_of_string
+  in
+  !nstrength > n
+
 let strengthen rwr pp obl =
+  incr nstrength;
   let en = pair_map List.hd pp in
-  if SimRel.entry en then
-    ()
+  if SimRel.entry en || too_strong () then
+    raise GiveUp
   else
     (* universally quantify over old obl states *)
     let vars =
@@ -53,7 +71,7 @@ let strengthen rwr pp obl =
         obl
     in
     en |> Rewrite.simrel_entry rwr
-       |> Common.snoc [str]
+       |> snoc [str]
        |> Logic.conj
        |> Logic.simplify
        |> Rewrite.update_simrel rwr en
@@ -67,20 +85,52 @@ let ck_paths rwr pp =
   let obl = obligation rwr pp in
   log pp obl;
   if Logic.valid Semantics.axioms obl then begin
-    Common.log "Passed.";
+    Common.log "[valid]";
     true
   end else begin
-    Common.log "Failed.";
+    Common.log "[invalid]";
     strengthen rwr pp obl;
     false
   end
 
+(* does path pair pp2 precede pp1 ? *)
+let precedes pp1 pp2 =
+  let ens =
+    pp1 |> pair_map List.hd
+        |> pair_map Prog.nid
+  in
+  let exs =
+    pp2 |> pair_map Common.last
+        |> pair_map Prog.nid
+  in
+  ens = exs
+
+let ck_entry rwr (pp, q) =
+  if ck_paths rwr pp then
+    q
+  else
+    (* push all path pairs that precede pp *)
+    rwr |> Rewrite.paths
+        |> List.filter (precedes pp)
+        |> List.fold_left UniQueue.push q
+
 let ck_rule rwr =
-  List.for_all
-    (ck_paths rwr)
-    rwr.Rewrite.paths
+  let rec loop q =
+    if UniQueue.is_empty q then
+      true
+    else
+      q |> UniQueue.pop
+        |> ck_entry rwr
+        |> loop
+  in
+  rwr |> Rewrite.paths
+      |> List.fold_left UniQueue.push UniQueue.empty
+      |> loop
 
 let check rwr =
-     ck_rule rwr
-  || ck_rule rwr
+  reset_nstrength ();
+  try
+    ck_rule rwr
+  with GiveUp ->
+    false
 
