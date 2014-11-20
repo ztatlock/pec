be0bf86ee2f0d06454fb723b11edc97ad7893ac6
Zach Tatlock <ztatlock@cs.washington.edu>
2014-11-20 03:31:35 -0800

    new semantics

--- src/semantics.ml
+++ src/semantics.ml
@@ -144,6 +144,243 @@ let vars_distinct (l, r) =
 (* background for reasoning about program executions *)
 
 let background = "
+
+;; ARRAYS
+
+(BG_PUSH
+  (FORALL (arr id val)
+    (EQ (select (store arr id val) id) val))
+)
+
+(BG_PUSH
+  (FORALL (id1 id2 arr val)
+    (IMPLIES (NEQ id1 id2)
+             (EQ (select (store arr id1 val) id2)
+                 (select arr id2))))
+)
+
+;; LOCATIONS
+
+(BG_PUSH
+  (FORALL (x y)
+    (NEQ (Orig x) (Temp y)))
+)
+
+;; STATES
+
+(BG_PUSH
+  (FORALL (o t)
+    (EQ (orig (state o t)) o))
+)
+
+(BG_PUSH
+  (FORALL (o t)
+    (EQ (temp (state o t)) t))
+)
+
+(DEFPRED (orig_eq state1 state2)
+  (EQ (orig state1)
+      (orig state2))
+)
+
+(DEFPRED (orig_equiv state1 state2)
+  (FORALL (id)
+    (EQ (lkup state1 (Orig id))
+        (lkup state2 (Orig id))))
+)
+
+(BG_PUSH
+  (FORALL (state1 state2)
+    (IMPLIES (orig_eq state1 state2)
+             (orig_equiv state2 state2)))
+)
+
+;; LKUP
+
+(BG_PUSH
+  (FORALL (state1 id)
+    (EQ (lkup state1 (Orig id))
+        (select (orig state1) id)))
+)
+
+(BG_PUSH
+  (FORALL (state1 id)
+    (EQ (lkup state1 (Temp id))
+        (select (temp state1) id)))
+)
+
+;; STEP
+
+(BG_PUSH
+  (FORALL (state1 id expr)
+    (EQ (step state1 (Assign (Orig id) expr))
+        (state (store (orig state1) id expr)
+               (temp state1))))
+)
+
+(BG_PUSH
+  (FORALL (state1 id expr)
+    (EQ (step state1 (Assign (Temp id) expr))
+        (state (orig state1)
+               (store (temp state1) id expr))))
+)
+
+(BG_PUSH
+  (FORALL (state1 state2 stmt)
+    (IMPLIES
+      (orig_equiv state1 state2)
+      (orig_equiv (step state1 stmt)
+                  (step state2 stmt))))
+)
+
+;; CODE PARAMS
+
+(BG_PUSH
+  (FORALL (state1 code)
+    (EQ (temp (step state1 (PCode code)))
+        (temp state1)))
+)
+
+(BG_PUSH
+  (FORALL (state1 code id)
+    (EQ (lkup (step state1 (PCode code)) (Temp id))
+        (lkup state1 (Temp id))))
+)
+
+;; EXPR PARAMS
+   
+(BG_PUSH
+  (FORALL (state1 state2 expr)
+    (IMPLIES (orig_equiv state1 state2)
+             (EQ (eval state1 (PExpr expr))
+                 (eval state2 (PExpr expr)))))
+)
+
+(BG_PUSH
+  (FORALL (state1 expr x)
+    (EQ (eval state1 (PExpr expr))
+        (eval (state (orig state1) x) (PExpr expr))))
+)
+
+;; SIDE CONDITIONS
+
+(DEFPRED (noread state1 expr var)
+  (FORALL (val)
+      (EQ (eval state1 expr)
+          (eval (step state1 (Assign var val)) expr)))
+)
+
+(BG_PUSH
+  (FORALL (state1 state2 expr var val)
+    (IMPLIES
+      (AND (EQ (step state1 (Assign var val)) state2)
+           (noread state2 expr var))
+      (EQ (eval state1 expr)
+          (eval state2 expr))))
+)
+
+(DEFPRED (nowrite state1 stmt var)
+  (EQ (lkup state1 var)
+      (lkup (step state1 stmt) var))
+)
+
+(DEFPRED (noaffect state1 stmt expr)
+  (EQ (eval state1 expr)
+      (eval (step state1 stmt) expr))
+)
+
+(DEFPRED (nodisturb stateCur stmt1 stmt2)
+  (FORALL (statePrev)
+    (IMPLIES (EQ stateCur (step statePrev stmt2))
+             (EQ (step (step stateCur stmt1) stmt2)
+                 (step stateCur stmt1))))
+)
+
+;; OPERATORS
+
+(BG_PUSH
+  (FORALL (expr)
+    (IMPLIES (NEQ 0 (Not expr))
+             (EQ 0 expr)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Add expr1 expr2)
+        (+ expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Sub expr1 expr2)
+        (- expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Mul expr1 expr2)
+        (* expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Div expr1 expr2)
+        (/ expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (IMPLIES (NEQ 0 (Lt expr1 expr2))
+             (< expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (IMPLIES (NEQ 0 (Lte expr1 expr2))
+             (<= expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (IMPLIES (NEQ 0 (Gt expr1 expr2))
+             (> expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (IMPLIES (NEQ 0 (Gte expr1 expr2))
+             (>= expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Not (Lt expr1 expr2))
+        (Gte expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Not (Lte expr1 expr2))
+        (Gt expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Not (Gt expr1 expr2))
+        (Lte expr1 expr2)))
+)
+
+(BG_PUSH
+  (FORALL (expr1 expr2)
+    (EQ (Not (Gte expr1 expr2))
+        (Lt expr1 expr2)))
+)
+
+;; END BACKGROUND
+
+"
+
+let old_background = "
 ;; STATE EQUIVALENCE
 
 (DEFPRED (orig_equiv state1 state2)
